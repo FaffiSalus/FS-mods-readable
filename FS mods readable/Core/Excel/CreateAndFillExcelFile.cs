de0aac1af25cec90b5829167a6224a8533bf6c93
FaffiSalus <pstrubig@icloud.com>
2026-07-07 08:11:37 +0200

    Support for excel sheets with more than 1m rows

--- FS mods readable/Core/Excel/CreateAndFillExcelFile.cs
+++ FS mods readable/Core/Excel/CreateAndFillExcelFile.cs
@@ -6,9 +6,24 @@ namespace FS_mods_readable.Core.Excel;
 
 public static class CreateAndFillExcelFile
 {
+    private const int _maxRows = 1000000;
+
     public static void CreateAndFill(string fileName, string sheetName, DataTable dataTable)
     {
+        if (dataTable.Rows.Count > _maxRows)
+        {
+            var tables = CreateDataTable.SplitTable(dataTable, _maxRows);
+            int i = 0;
+            foreach (var table in tables)
+            {
+                Export2Excel(fileName, sheetName + "_" + i, table);
+                i++;
+            }
+        }
+        else
+        {
             Export2Excel(fileName, sheetName, dataTable);
+        }
     }
     
     private static void Export2Excel(string fileName, string sheetName, DataTable dataTable)
